2a53e87513b4d7c0ed31e15a8374483615aa11e0
Iggy <up_isms@yahoo.com>
2018-07-16 03:08:57 -0700

    app

--- app/src/main/java/com/example/iggy/beautyapp/MainActivity.java
+++ app/src/main/java/com/example/iggy/beautyapp/MainActivity.java
@@ -1,12 +1,13 @@
 package com.example.iggy.beautyapp;
 
 import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by Iggy on 7/16/2018.
  */
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
 
 
